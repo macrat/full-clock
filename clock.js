6de728e8997f10692ada630ae8b1711a87b4dbfa
MacRat <m@crat.jp>
2020-03-28 00:03:34 +0900

    Fixed bug on 0:00

--- clock.js
+++ clock.js
@@ -20,17 +20,17 @@ const rendering = () => {
         analog.classList.remove('stop-motion');
     }
 
-    let hour = now.getHours()%12 / 12;
-    if (hour === 0 && now.getMilliseconds() < 500) hour = 1;
-    hourLines.map(elm => elm.setAttribute('transform', `rotate(${hour * 360})`));
-
-    let minute = now.getMinutes()/60;
-    if (minute === 0 && now.getMilliseconds() < 500) minute = 1;
-    minuteLines.map(elm => elm.setAttribute('transform', `rotate(${minute * 360})`));
-
     let second = now.getSeconds()/60;
     if (second === 0 && now.getMilliseconds() < 500) second = 1;
     secondLine.setAttribute('transform', `rotate(${second * 360})`);
+
+    let minute = now.getMinutes()/60;
+    if (minute === 0 && second === 0 && now.getMilliseconds() < 500) minute = 1;
+    minuteLines.map(elm => elm.setAttribute('transform', `rotate(${minute * 360})`));
+
+    let hour = now.getHours()%12 / 12;
+    if (hour === 0 && minute === 0 && second === 0 && now.getMilliseconds() < 500) hour = 1;
+    hourLines.map(elm => elm.setAttribute('transform', `rotate(${hour * 360})`));
 
     const smoothSecond = now.getSeconds()/60 + now.getMilliseconds()/1000/60;
     if (smoothSecond === 0 && now.getMilliseconds() < 500) smoothSecond = 1;
